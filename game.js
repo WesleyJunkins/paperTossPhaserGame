5ac39f0d518814e93bb4eecff7d7e50658f6560f
WesleyJunkins <Wjunkins13@yahoo.com>
2023-02-26 21:16:29 -0600

    new stuff

--- game.js
+++ game.js
@@ -19,10 +19,14 @@ var game = new Phaser.Game(config);
 
 function preload (){
     this.load.image('background', 'assets/background.png');
+    this.load.image('trashCan', 'assets/trashCan.png');
+    //this.load.spritesheet('paper', 'assets/paper.png', 38, 48);
+    
 }
 
 function create(){
     this.add.image(400, 300, 'background');
+    //myPaper = this.game.add.sprite
 }
 
 function update() {
