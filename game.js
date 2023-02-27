766f5640afa8903f3f11c4b443bcf460f15e9e41
WesleyJunkins <Wjunkins13@yahoo.com>
2023-02-27 13:53:39 -0600

    added more objects

--- game.js
+++ game.js
@@ -5,7 +5,8 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
+            gravity: { y: 300 },
+            debug: false
         }
     },
     scene: {
@@ -16,17 +17,25 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+paper = this.physics.add.sprite(100, 450, 'paper');
+paper.setBounce(0.2);
+paper.setCollideWorldBounds(true);
 
 function preload (){
     this.load.image('background', 'assets/background.png');
     this.load.image('trashCan', 'assets/trashCan.png');
-    //this.load.spritesheet('paper', 'assets/paper.png', 38, 48);
+    this.load.spritesheet('paper', 'assets/paper.png', {frameWidth: 32, frameHeight: 48});
+    this.load.image('ground', 'assets/ground.png');
     
 }
 
 function create(){
     this.add.image(400, 300, 'background');
-    //myPaper = this.game.add.sprite
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 700, 'ground').setScale(1).refreshBody();
+    this.load.sprite
+    // cannonBall = this.add.sprite(500, 700, 'cannonBall');
+    // cannonBall.scale.setTo(0.5, 0.5);
 }
 
 function update() {
