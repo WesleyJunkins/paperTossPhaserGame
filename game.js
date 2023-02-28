6d09ef4c60138c9769880e56d2e89eb482b6e800
WesleyJunkins <67446010+WesleyJunkins@users.noreply.github.com>
2023-02-27 21:47:29 -0600

    Added new physics and collision detection. This still needs work tho.

--- game.js
+++ game.js
@@ -3,7 +3,13 @@ var config = {
     width: 1000,
     height: 700,
     physics: {
-        default: 'matter'
+        default: 'matter',
+        arcade: {
+            debug: true
+        },
+        matter: {
+            debug: true
+        }
     },
     scene: {
         preload: preload,
@@ -35,9 +41,19 @@ function create(){
 
     var canDrag = this.matter.world.nextGroup();
 
-    this.matter.add.image(100, 100, 'ball', null, { chamfer: 16 }).setBounce(0.9).setCollisionGroup(canDrag);
+    this.matter.add.image(400, 500, 'ball', null, { chamfer: 16 }).setScale(0.5).setBounce(0.9).setCollisionGroup(canDrag);
 
     this.matter.add.mouseSpring({length: 1, stiffness: 0.6, collisionFilter: {group: canDrag}});
+
+    this.matter.add.image(580, 520, 'trashCan').setScale(1.7).setStatic(true);
+
+    this.matter.world.on('collisionstart', function (event, bodyA, bodyB) {
+
+        console.log("Hit");
+        // bodyA.gameObject.setTint(0xff0000);
+        // bodyB.gameObject.setTint(0x00ff00);
+
+    });
 
 
 
@@ -112,3 +128,86 @@ function update() {
 
 //     this.matter.add.mouseSpring({ length: 1, stiffness: 0.6, collisionFilter: { group: canDrag } });
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var config = {
+//     type: Phaser.AUTO,
+//     width: 800,
+//     height: 600,
+//     backgroundColor: '#1b1464',
+//     parent: 'phaser-example',
+//     physics: {
+//         default: 'matter',
+//         matter: {
+//             gravity: {
+//                 x: 0,
+//                 y: 0
+//             }
+//         }
+//     },
+//     scene: {
+//         preload: preload,
+//         create: create
+//     }
+// };
+
+// var game = new Phaser.Game(config);
+
+// function preload ()
+// {
+//     this.load.image('block', 'assets/sprites/block.png');
+// }
+
+// function create ()
+// {
+//     var blockA = this.matter.add.image(0, 300, 'block').setBounce(1).setFriction(0);
+
+//     var blockB = this.matter.add.image(600, 300, 'block').setStatic(true);
+
+//     blockA.setVelocityX(10);
+
+//     this.matter.world.on('collisionstart', function (event, bodyA, bodyB) {
+
+//         bodyA.gameObject.setTint(0xff0000);
+//         bodyB.gameObject.setTint(0x00ff00);
+
+//     });
+// }
+
+
